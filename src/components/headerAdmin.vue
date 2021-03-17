2a9699efc911c241f564ab30f12da2162c947870
carlo <carlosarce800@gmail.com>
2021-03-17 13:15:46 -0600

    Implementación de axios y menú de hamburguesa

--- src/components/headerAdmin.vue
+++ src/components/headerAdmin.vue
@@ -9,16 +9,37 @@
             alt=""
           />
         </div>
-        <nav>
-          <a href="/Admin">Productos</a>
-          <a href="/Inventario">Inventario</a>
-          <a href="/CajeroAdmin">Cajeros</a>
-          <a href="/Marcas">Marcas</a>
-          <a href="/Categorias">Categorías</a>
-          <a href="/Ventas">Ventas</a>
-          <a href="/Paquetes">Paquetes</a>
-          <a href="/Descuentos">Descuentos</a>
-        </nav>
+        <div class="ui grid">
+          <div class="computer only row">
+            <div class="column">
+              <nav>
+                <a class="item" href="/Admin">Productos</a>
+                <a class="item" href="/Inventario">Inventario</a>
+                <a class="item" href="/CajeroAdmin">Cajeros</a>
+                <a class="item" href="/Marcas">Marcas</a>
+                <a class="item" href="/Categorias">Categorías</a>
+                <a class="item" href="/Ventas">Ventas</a>
+                <a class="item" href="/Paquetes">Paquetes</a>
+                <a class="item" href="/Descuentos">Descuentos</a>
+              </nav>
+            </div>
+          </div>
+          <div class="tablet mobile only row">
+            <div class="column">
+              <div class="menuHamburguesa">
+                <a id="mobile_item" class="item"><i class="bars icon"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="ui sidebar vertical menu">
+            <a class="item">Menu Item A</a>
+            <a class="item">Menu Item B</a>
+            <a class="item">Menu Item C</a>
+            <a class="item">Menu Item D</a>
+          </div>
+        </div>
       </div>
     </header>
   </div>
@@ -64,4 +85,34 @@ header nav a:hover {
   background: #64b5f6;
   border-radius: 50px;
 }
+
+/* Menú de hamburguesa */
+#content {
+  min-height: 100px;
+}
+.ui.grid {
+  padding: 0 !important;
+}
+.pushable.segment {
+  margin: 0 !important;
+}
+.menuHamburguesa {
+  display: flex;
+  padding: 10px 20px;
+  margin-top: 5%;
+  max-width: 60px;
+  background: white;
+  text-shadow: 1px 1px black;
+  float: right;
+}
 </style>
+
+<script>
+/* const $ = require("jquery");
+$(".ui.sidebar")
+  .sidebar({
+    context: $(".ui.pushable.segment"),
+    transition: "overlay",
+  })
+  .sidebar("attach events", "#mobile_item"); */
+</script>
--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import SuiVue from "semantic-ui-vue";
 import "semantic-ui-css/semantic.min.css";
-import router from './router'
+import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
 Vue.config.productionTip = false;
 Vue.use(SuiVue);
+Vue.use(VueAxios, axios);
 
 new Vue({
   router,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
